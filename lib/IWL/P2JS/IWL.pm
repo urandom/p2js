dd37b07653d46d258a82a33baa3c94f5009ac937
vkojouharov <vkojouharov@d3eaf211-ee32-0410-a207-cdbac743c15d>
2008-02-04 08:58:01 +0000

    forgotten file
    git-svn-id: http://iwl.googlecode.com/svn/p2js/trunk@529 d3eaf211-ee32-0410-a207-cdbac743c15d

---
+++ lib/IWL/P2JS/IWL.pm
@@ -0,0 +1,118 @@
+#! /bin/false
+# vim: set autoindent shiftwidth=4 tabstop=8:
+
+package IWL::P2JS::IWL;
+
+use strict;
+
+use IO::Handle;
+
+my $p2js;
+
+=head1 NAME
+
+IWL::P2JS::IWL - a plugin for P2JS to handle IWL specific tasks
+
+=head1 DESCRIPTION
+
+IWL::P2JS::IWL is a plugin, whose task is to overload specific L<IWL> functions, so that they use L<IWL::P2JS>.
+
+=head1 CONSTRUCTOR
+
+IWL::P2JS::IWL->new(B<IWL::P2JS>)
+
+where B<IWL::P2JS> is an instantiated object from the L<IWL::P2JS> class
+
+=cut
+
+sub new {
+    my ($proto, $converter) = @_;
+    my $class = ref($proto) || $proto;
+    my $self = bless {p2js => ($p2js = $converter)}, $class;
+
+    return $self->__inspectInc;
+}
+
+# Internal
+#
+my %monitors = qw(IWL/Script.pm 1);
+
+sub __inspectInc {
+    my $self = shift;
+    do { $self->__connect($_) if $INC{$_} } foreach keys %monitors;
+
+    $self->{__connector} = sub {
+        my ($coderef, $filename) = @_;
+        my $result;
+        @INC = grep {$_ ne $self->{__connector}} @INC;
+        require $filename;
+        $self->__connect($filename) if $monitors{$filename};
+        unshift @INC, $self->{__connector};
+
+        my $fh = IO::Handle->new;
+        tie *fh, 'IWL::P2JS::IWL::_FH';
+        return *fh;
+    };
+    unshift @INC, $self->{__connector};
+
+    return $self;
+}
+
+sub __connect {
+    my ($self, $filename) = @_;
+
+    if ($filename eq 'IWL/Script.pm') {
+        $self->__iwlScriptInit;
+    }
+
+    return $self;
+}
+
+# IWL::Script
+#
+
+sub __iwlScriptInit {
+    my $self = shift;
+
+    *setScript = *IWL::Script::setScript;
+    delete $::{'IWL::'}{'Script::'}{'setScript'};
+
+    no warnings qw(redefine);
+    sub IWL::Script::setScript {
+        my ($self, $param) = @_;
+        if (ref $param eq 'CODE') {
+            *setScript{CODE}->($self, $p2js->covert($param));
+        } else {
+            *setScript{CODE}->($self, $param);
+        }
+    }
+}
+
+package IWL::P2JS::IWL::_FH;
+
+sub TIEHANDLE {
+	my $self = {};
+	bless $self, shift;
+
+	return $self;
+}
+
+sub READ {
+	my $self = shift;
+	$_[0] = "1;";
+}
+
+1;
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2006-2007  Viktor Kojouharov. All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See perldoc perlartistic.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+=cut
